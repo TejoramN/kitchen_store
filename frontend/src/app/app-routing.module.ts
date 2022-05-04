48266ab33c404f99a68576cabeeb1079c3bdaff9
Nandimandalam Tejoram <tejoram374@gmail.com>
2022-05-04 15:55:32 +0530

    kitchen Store

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ItemPageComponent } from './components/pages/item-page/item-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'search/:searchTerm',component:HomeComponent},
+  {path:'item/:id',component:ItemPageComponent},
+  {path:'cart-page', component: CartPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { SearchComponent } from './components/partials/search/search.component';
+import { ItemPageComponent } from './components/pages/item-page/item-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    ItemPageComponent,
+    CartPageComponent,
+    TitleComponent,
   ],
   imports: [
     BrowserModule,
